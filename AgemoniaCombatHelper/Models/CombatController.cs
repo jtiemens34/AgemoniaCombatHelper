61a47a8cbab8470f4d73b46c0e4fa99d7ccc099b
jtiemens34 <jtiemens34@hotmail.com>
2024-05-27 15:10:59 -0700

    Refresh tracker when new entities are added, and made Add Hero Modal close on submit.

--- AgemoniaCombatHelper/Models/CombatController.cs
+++ AgemoniaCombatHelper/Models/CombatController.cs
@@ -1,18 +1,20 @@
-﻿using System.Net.Http.Json;
-using System.Text.Json;
-using Microsoft.AspNetCore.Components;
+﻿using AgemoniaCombatHelper.Pages;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 namespace AgemoniaCombatHelper.Models;
 public class CombatController
 {
     private List<Hero> Heroes;
     private List<Enemy> Enemies;
-    private List<Entity> TurnOrder;
+    public List<Entity> TurnOrder;
+
+    private CombatTracker CombatTracker;
+
+    public delegate void TurnOrderUpdated();
 
     public InitiativeCard? ActiveCard { get; private set; }
     private InitiativeDeck? InitiativeCards;
     private InitiativeDeck? Discard;
-
 
     public CombatController()
     {
@@ -23,8 +25,15 @@ public class CombatController
     public void DrawNewCard()
     {
         ActiveCard = InitiativeCards?.Data?[0];
+        ArrangeEntities();
+    }
+
+    private void ArrangeEntities()
+    {
+        // TODO: Change logic to handle multiple entities from the same action color.
         if (ActiveCard is null) return;
         if (ActiveCard.Actions is null) return;
+        TurnOrder.Clear();
         foreach (Action action in ActiveCard.Actions)
         {
             if (action.EntityType == EntityType.Hero && Heroes.Any(h => h.ActionColor == action.ActionColor))
@@ -45,11 +54,15 @@ public class CombatController
     public void AddHero(Hero hero)
     {
         Heroes.Add(hero);
+        ArrangeEntities();
+        CombatTracker?.Refresh();
     }
 
     public void AddEnemy(Enemy enemy)
     {
         Enemies.Add(enemy);
+        ArrangeEntities();
+        CombatTracker?.Refresh();
     }
 
     public List<Entity> GetEntities()
@@ -60,5 +73,14 @@ public class CombatController
     public void SetCardsFromJson(InitiativeDeck deck)
     {
         InitiativeCards = deck;
+    }
+
+    public void SetCombatTracker(CombatTracker tracker)
+    {
+        CombatTracker = tracker;
+    }
+    public void RefreshDisplay()
+    {
+        CombatTracker.Refresh();
     }
 }
