69b22cbdd27fb4fafe354b089eecb63ad8990204
jtiemens34 <jtiemens34@hotmail.com>
2024-05-30 15:05:42 -0700

    Handle shuffling and manipulating initiative cards properly

--- AgemoniaCombatHelper/Models/CombatController.cs
+++ AgemoniaCombatHelper/Models/CombatController.cs
@@ -5,32 +5,57 @@ public class CombatController
 {
     private List<Hero> Heroes;
     private List<Enemy> Enemies;
-    private Scenarios Scenarios;
+    private Scenarios? Scenarios;
     public List<Entity> TurnOrder;
 
-    private CombatTracker CombatTracker;
+    private CombatTracker? CombatTracker;
 
     public delegate void TurnOrderUpdated();
 
     public InitiativeCard? ActiveCard { get; private set; }
     private InitiativeDeck? InitiativeCards;
     private InitiativeDeck? Discard;
+    private Random rng = new Random();
 
     public CombatController()
     {
         Heroes = [];
         Enemies = [];
         TurnOrder = [];
+        Discard = new()
+        {
+            Data = []
+        };
     }
     public void DrawNewCard()
     {
-        ActiveCard = InitiativeCards?.Data?[0];
+        // Do we need to shuffle?
+        if (Discard.Data.Exists(c => c.Shuffle == true))
+        {
+            InitiativeCards.Data.AddRange(Discard.Data);
+            Discard.Data.Clear();
+            Shuffle();
+        }
+        if (ActiveCard is not null) Discard.Data.Add(ActiveCard);
+        ActiveCard = InitiativeCards!.Data![0];
+        InitiativeCards.Data.RemoveAt(0);
         ArrangeEntities();
     }
-
-    private void ArrangeEntities()
+    private void Shuffle()
     {
-        // TODO: Change logic to handle multiple entities from the same action color.
+        int n = InitiativeCards.Data.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            InitiativeCard value = InitiativeCards.Data[k];
+            InitiativeCards.Data[k] = InitiativeCards.Data[n];
+            InitiativeCards.Data[n] = value;
+        }
+    }
+
+    public void ArrangeEntities()
+    {
         TurnOrder.Clear();
         // If no card has been drawn, simply add enemies and heroes
         if (ActiveCard is null)
@@ -49,11 +74,11 @@ public class CombatController
         {
             if (action.EntityType == EntityType.Hero && Heroes.Any(h => h.ActionColor == action.ActionColor))
             {
-                TurnOrder.Add(Heroes.Find(h => h.ActionColor == action.ActionColor));
+                TurnOrder.AddRange(Heroes.FindAll(h => h.ActionColor == action.ActionColor));
             }
             if (action.EntityType == EntityType.Enemy && Enemies.Any(e => e.ActionColor == action.ActionColor))
             {
-                TurnOrder.Add(Enemies.Find(e => e.ActionColor == action.ActionColor));
+                TurnOrder.AddRange(Enemies.FindAll(e => e.ActionColor == action.ActionColor));
             }
         }
         foreach (Hero unusedHero in Heroes)
@@ -86,6 +111,7 @@ public class CombatController
     public void SetCardsFromJson(InitiativeDeck deck)
     {
         InitiativeCards = deck;
+        Shuffle();
     }
     public void SetScenariosFromJson(Scenarios scenarios)
     {
